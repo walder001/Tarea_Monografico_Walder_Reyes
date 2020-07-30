145be22f17c248e7d2b9c3c78d25be18fef75e06
Walder Reyes De Jesus <walder_reyes@ucne.edu.do>
2020-07-30 11:47:17 -0400

    Repo

---
+++ Tarea_Monografico_Walder_Reyes/Repositorio/Base/IRepoWrapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tarea_Monografico_Walder_Reyes.Repositorio.Base
+{
+    public interface IRepoWrapper
+    {
+        //Solo colocamos las propiedades tipo get.
+        IEmpleadoRepo Empleado { get; }
+        IEstudianteRepo Estudiante { get; }
+
+        IProfesorRepo Profesor { get; }
+
+
+    }
+}
---
+++ Tarea_Monografico_Walder_Reyes/Repositorio/Base/RepoWrapper.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tarea_Monografico_Walder_Reyes.Models;
+
+namespace Tarea_Monografico_Walder_Reyes.Repositorio.Base
+{
+    public class RepoWrapper : IRepoWrapper
+    {
+        private readonly AppDbContext _repoContext;
+
+        private IEstudianteRepo _estudiente;
+        private IProfesorRepo _profesor;
+        private IEmpleadoRepo _empleado;
+
+
+        public RepoWrapper(AppDbContext repoContext)
+        {
+            _repoContext = repoContext;
+        }
+        public IEstudianteRepo Estudiante
+        {
+            get
+            {
+                if (_estudiente == null)
+                {
+                    _estudiente = new EstudienteRepo(_repoContext);
+                }
+
+                return _estudiente;
+            }
+        }
+
+        //Hacer lo mismo que con Marca
+        public IProfesorRepo Profesor
+        {
+            get
+            {
+                if (_profesor == null)
+                {
+                    _profesor = new ProfesorRepo(_repoContext);
+
+
+                }
+
+                return _profesor;
+            }
+        }
+        public IEmpleadoRepo Empleado
+        {
+            get
+            {
+                if (_empleado == null)
+                {
+                    _empleado = new EmpleadoRepo(_repoContext);
+                 }
+
+                return _empleado;
+            }
+        }
+      
+    }
+}
